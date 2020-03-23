c8871c804091ec934f8482f7f1e8f82c71bfc670
-Tete- Css <nestor.informatica95@gmail.com>
2020-03-23 17:55:38 +0000

    Gallery added but i dont like

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,7 @@ var data;
 var selection;
 var storageRef;
 var fichero;
+var fichero2;
 var email = "invitado";
 
 function initialize() {
@@ -15,8 +16,11 @@ function initialize() {
   downloadTechs();
   loadButtons();
   downloadNews();
+  downloadPhotos();
+  downloadPets();
   storageRef = firebase.storage().ref();
   fichero = document.getElementById("mdl-add-img");
+  fichero2 = document.getElementById("img-photo");
 
   document.getElementById("all-content").style.display = "none";
   document.getElementById("to-signin").style.display = "unset";
@@ -33,11 +37,16 @@ function loadButtons() {
   document.getElementById("form-login-mdl").addEventListener("submit", logIn);
   document.getElementById("log-out-btn").addEventListener("click", logOut);
   document.getElementById("clicked-petis").addEventListener("click", petiZoneWind);
+  document.getElementById("close-subs-mdl").addEventListener("click", closepetiZoneWind);
   document.getElementById("news-btn").addEventListener("click", showTakedaNews);
   document.getElementById("techs-btn").addEventListener("click", showTechsList);
   document.getElementById("add-news-btn").addEventListener("click", openAddNewsMdl);
   document.getElementById("add-news-mdl-cls").addEventListener("click", closeAddNewsMdl);
   document.getElementById("form-add-news-mdl").addEventListener("submit", AddNewsToList);
+  document.getElementById("gallery-btn").addEventListener("click", showPhotoGallery);
+  document.getElementById("add-photo-btn").addEventListener("click", openPhotoModal);
+  document.getElementById("close-photo-modal-btn").addEventListener("click", closePhotoModal);
+  document.getElementById("photo-modal").addEventListener("submit", AddPhotoToList);
 }
 
 function downloadTechs() {
@@ -174,10 +183,13 @@ function showTec(snap) {
     var deleteButtons = document.getElementsByClassName("delete");
     for (var i = 0; i < deleteButtons.length; i++) {
       deleteButtons[i].addEventListener("click", deleteTech);
+    }
+    for (var i = 0; i < editButtons.length; i++) {
       editButtons[i].addEventListener("click", editTech);
     }
     document.getElementById("news-btn").style.display = "block";
     document.getElementById("techs-btn").style.display = "block";
+    document.getElementById("gallery-btn").style.display = "block";
     document.getElementById("login-btn").style.display = "none";
     document.getElementById("log-out-btn").style.display = "block";
   } else {//end If email != invitado
@@ -355,18 +367,31 @@ function searchTech() {
 }
 
 function petiZoneWind() {
-  window.location.href = "petitionsWindow.html";
+  document.getElementById("subs-modal").style.display = "unset";
+  downloadPets();
 }
+
+function closepetiZoneWind(){
+  document.getElementById("subs-modal").style.display = "none";
+}
+
+//thi below is the Js for the news
 
 function showTakedaNews() {
   document.getElementById("news-content").style.display = "block";
   document.getElementById("all-content").style.display = "none";
+  document.getElementById("photo-content").style.display = "none";
+  document.getElementById("add-photo-btn").style.display = "none";
+  downloadNews();
 }
 
 function showTechsList() {
   document.getElementById("news-content").style.display = "none";
   document.getElementById("add-news-btn").style.display = "none";
   document.getElementById("all-content").style.display = "unset";
+  document.getElementById("photo-content").style.display = "none";
+  document.getElementById("add-photo-btn").style.display = "none";
+  downloadTechs();
 }
 
 function downloadNews() {
@@ -386,6 +411,7 @@ function showNews(snap) {
       document.getElementById("to-signin").style.display = "none";
       document.getElementById("subs-peti-zone").style.display = "block";
       document.getElementById("add-news-btn").style.display = "unset";
+      document.getElementById("subs-peti-zone").style.display = "block";
       for (var key in data) {
         console.log("Admin")
         rows +=
@@ -394,7 +420,7 @@ function showNews(snap) {
           '<h4>' + data[key].title + '<small>&nbsp;&nbsp;<i>' + data[key].date + '</i></small></h4>' +
           '<p>' + data[key].body + '</p>' +
           '<div class="row">' +
-         // '<i  class="fas fa-ellipsis-h ml-3 plask" data-news="' + key + '"></i>' +
+          // '<i  class="fas fa-ellipsis-h ml-3 plask" data-news="' + key + '"></i>' +
           '<div id="sniegel">' +
           '<i class="far fa-trash-alt delete ml-3" data-news="' + key + '"></i>' +
           '<i class="far fa-edit edit " data-news="' + key + '"></i>' +
@@ -436,10 +462,13 @@ function showNews(snap) {
     var deleteButtons = document.getElementsByClassName("delete");
     for (var i = 0; i < deleteButtons.length; i++) {
       deleteButtons[i].addEventListener("click", deleteNews);
+    }
+    for (var i = 0; i < editButtons.length; i++) {
       editButtons[i].addEventListener("click", editNews);
     }
     document.getElementById("news-btn").style.display = "block";
     document.getElementById("techs-btn").style.display = "block";
+    document.getElementById("gallery-btn").style.display = "block";
     document.getElementById("login-btn").style.display = "none";
     document.getElementById("log-out-btn").style.display = "block";
   } else {//end If email != invitado
@@ -494,7 +523,7 @@ function AddNewsToList(event) {
     var body = formNews.Nbody.value;
     date = new Date();
     var meses = new Array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
-    date = date.getDate()+"/" + meses[date.getMonth()] +"/"+  date.getFullYear();
+    date = date.getDate() + "/" + meses[date.getMonth()] + "/" + date.getFullYear();
     firebase.database().ref("news/").push({
       title,
       body,
@@ -531,14 +560,14 @@ function closeAddNewsMdl() {
 
 function openAddNewsMdl() {
   console.log("modal abierto")
-  if(operation == ADD){
+  if (operation == ADD) {
     document.getElementById("add-news-mdl-btn").style.display = "unset";
     document.getElementById("edit-news-btn").style.display = "none";
-  }else{
+  } else {
     document.getElementById("add-news-mdl-btn").style.display = "none";
     document.getElementById("edit-news-btn").style.display = "unset";
   }
-  
+
   document.getElementById("mdl-add-news").style.display = "unset";
 }
 
@@ -551,21 +580,284 @@ function resetNewsForm() {
   operation = ADD;
 }
 
+//this below is JS for the photo gallery
 
-// function showDelEdit(event) {
-//   var buttonClicked = event.target;
-//   var key = buttonClicked.getAttribute("data-news");
-//   var site1 = document.getElementById("sniegel");
-//   var btnns = document.getElementsByClassName("plask");
+function openPhotoModal() {
+  document.getElementById("photo-modal").style.display = "unset";
+}
 
-// var content = 
-// '<i class="far fa-trash-alt delete ml-3 wiwi" data-news="' + key + '"></i>' +
-// '<i class="far fa-edit edit wiwi" data-news="' + key + '"></i>';
+function closePhotoModal() {
+  document.getElementById("photo-modal").style.display = "none";
+}
 
-// for (var j = 0; j < btnns.length; j++) {
-//   btnns[j] = content;
-// }
+function downloadPhotos() {
+  var TakPhoto = firebase.database().ref("photo-gallery");
+  TakPhoto.on("value", showPhotos);
+}
 
-// site1.innerHTML = content;
+function showPhotos(snap) {
+  console.log("Viendo fotos")
+  data = snap.val();
+  var rows = "";
 
-// }
+  if (email != "invitado") {
+    if (email == "admin@admin.com") {
+      document.getElementById("all-content").style.display = "none";
+      document.getElementById("news-content").style.display = "none";
+      document.getElementById("photo-content").style.display = "unset";
+      document.getElementById("to-signin").style.display = "none";
+      document.getElementById("subs-peti-zone").style.display = "block";
+      document.getElementById("add-news-btn").style.display = "none";
+      for (var key in data) {
+        //ESTOY CON EL SIGUIENTE PROBLEMA Y ES QUE ME SALE UNA ENCIMA DE OTRA 
+        console.log("Admin")
+        rows +=
+        '<div class="col-sm-3">' +
+          '<div class="card mt-2">' +
+          '<img class="card-img-top" src="' + data[key].url + '" alt="Card image" style="width:100%">' +
+          '<p class="card-title-photo">' + data[key].title + '</p>' +
+          '<div class="card-body">' +
+          '<div class="row">' +
+          '<i class="far fa-trash-alt delete" data-photo="' + key + '"></i>' +
+          '<i class="far fa-edit edit" data-photo="' + key + '"></i>' +
+          '</div>' +
+          '</div>' +
+          '</div>' +
+          '</div>'
+      }
+
+    } else {
+
+      //aqui iria un if si el email es igual al email que subio la imagen , que te deje borrar y edita
+      document.getElementById("all-content").style.display = "none";
+      document.getElementById("news-content").style.display = "none";
+      document.getElementById("photo-content").style.display = "unset";
+      document.getElementById("to-signin").style.display = "none";
+
+      //if (data[key].upEmail == email) {
+        for (var key in data) {
+          console.log("Normal")
+          rows +=
+          '<div class="col-sm-3">' +
+            '<div class="card shadow bg-white mt-2">' +
+            '<img class="card-img-top" src="' + data[key].url + '" alt="Card image" style="width:100%">' +
+            '<p class="card-title-photo">' + data[key].title + '</p>' +
+            '<div class="card-body">' +
+            '<div class="row">' +
+           // '<i class="far fa-trash-alt delete" data-photo="' + key + '"></i>' +
+           // '<i class="far fa-edit edit" data-photo="' + key + '"></i>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>'
+        }
+      /*} else {
+        for (var key in data) {
+          console.log("Normal")
+          rows +=
+            '<div class="card">' +
+            '<img class="card-img-top" src="' + data[key].url + '" alt="Card image" style="width:100%">' +
+            '<p class="card-title-photo">' + data[key].title + '</p>' +
+            '<div class="card-body">' +
+            '<div class="row">' +
+            '<i class="far fa-trash-alt delete" data-photo="' + key + '"></i>' +
+            '<i class="far fa-edit edit" data-photo="' + key + '"></i>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+        }
+      }*/
+    }
+
+
+    var photoCont = document.getElementById("photo-content");
+    photoCont.innerHTML = rows;
+
+    var editButtons = document.getElementsByClassName("edit");
+    var deleteButtons = document.getElementsByClassName("delete");
+    for (var i = 0; i < deleteButtons.length; i++) {
+      deleteButtons[i].addEventListener("click", deletePhoto);
+    }
+    for (var i = 0; i < editButtons.length; i++) {
+      editButtons[i].addEventListener("click", editPhoto);
+    }
+    document.getElementById("news-btn").style.display = "block";
+    document.getElementById("techs-btn").style.display = "block";
+    document.getElementById("gallery-btn").style.display = "block";
+    document.getElementById("login-btn").style.display = "none";
+    document.getElementById("log-out-btn").style.display = "block";
+    document.getElementById("add-photo-btn").style.display = "unset";
+  } else {//end If email != invitado
+    document.getElementById("all-content").style.display = "none";
+    document.getElementById("to-signin").style.display = "unset";
+  }
+  userLogged();
+}
+
+
+function deletePhoto(event) {
+  var buttClick = event.target;
+  var keyPhotoToDelete = buttClick.getAttribute("data-photo");
+  var refPhotoToDelete = firebase.database().ref("photo-gallery/" + keyPhotoToDelete);
+  refPhotoToDelete.remove();
+}
+
+function editPhoto(event) {
+  console.log("Estoy en edit phot");
+
+  document.getElementById("").style.display = "none";
+  document.getElementById("").style.display = "unset";
+  operation = UPDATE;
+
+  var buttonClicked = event.target;
+
+  var formPhoto = document.getElementById("form-photo");
+  keyPhotoToEdit = buttonClicked.getAttribute("data-photo");
+  var refPhotoToEdit = firebase.database().ref("photo-gallery/" + keyPhotoToEdit);
+  console.log("La key de la tecnica apunto de editar es: " + keyPhotoToEdit)
+
+  refPhotoToEdit.once("value", function (snap) {
+    var data = snap.val();
+    formPhoto.title.value = data.title
+  });
+  openPhotoModal();
+}
+
+function AddPhotoToList(event) {
+  console.log("foto revisando si es edit o enviar");
+
+  event.preventDefault();
+
+  var formPhoto = event.target;
+
+  if (operation == ADD) {
+    console.log("Revisado para enviar");
+    document.getElementById("add-photo-mdl-btn").style.display = "unset";
+    document.getElementById("edit-photo-mdl-btn").style.display = "none";
+
+    var title = formPhoto.Imgtitle.value;
+    var imagenASubir = fichero2.files[0];
+    var FType = imagenASubir.type;
+    var upEmail = email;
+
+    if (imagenASubir.type == "image/jpeg") {
+      console.log("imagen")
+      var uploadTask = storageRef.child("photo-gallery/image/" + imagenASubir.name).put(imagenASubir);
+      uploadTask.on("state_changed",
+        function (snapshot) {
+          console.log("primer")
+        }, function (error) {
+          console.log(error);
+        }, function () {
+          console.log("funciona")
+          uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+            console.log('File available at', downloadURL);
+            firebase.database().ref("photo-gallery/").push({
+              url: downloadURL,
+              title,
+              FType,
+              upEmail
+            });
+          });
+        });
+    } else {
+      console.log("video")
+      var uploadTask = storageRef.child("photo-gallery/video/" + imagenASubir.name).put(imagenASubir);
+      uploadTask.on("state_changed",
+        function (snapshot) {
+          console.log("primer")
+        }, function (error) {
+          console.log(error);
+        }, function () {
+          console.log("funciona")
+          uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+            console.log('File available at', downloadURL);
+            firebase.database().ref("photo-gallery/").push({
+              url: downloadURL,
+              title,
+              FType,
+              upEmail
+            });
+          });
+        });
+    }
+
+    operation = ADD;
+
+    document.getElementById("add-mdl").style.display = "none";
+  } else {
+
+    console.log("Revisado para editar")
+
+    var refPhoto = firebase.database().ref("photo-gallery/" + keyPhotoToEdit);
+    console.log("La key de la foto recien editado es: " + keyPhotoToEdit);
+
+    refPhoto.update({
+      title: formPhoto.title.value
+    });
+
+    document.getElementById("edit-photo-mdl-btn").style.display = "unset";
+    document.getElementById("add-photo-mdl-btn").style.display = "none";
+
+    operation = ADD;
+    document.getElementById("photo-modal").style.display = "none";
+  }
+  resetPhotoForm();
+}
+
+function resetPhotoForm() {
+  console.log("form foto reseteado");
+  var formRes = document.getElementById("form-add-news-mdl");
+  document.getElementById("add-news-btn").style.display = "unset";
+  document.getElementById("edit-news-btn").style.display = "none";
+  formRes.reset();
+  operation = ADD;
+}
+
+function showPhotoGallery(){
+  document.getElementById("all-content").style.display = "none";
+  document.getElementById("news-content").style.display = "none";
+  document.getElementById("photo-content").style.display = "unset";
+  downloadPhotos();
+}
+
+
+
+//modal petitions below
+
+function downloadPets() {
+  var petis = firebase.database().ref("/subscriptions");
+  petis.on("value", showPets);
+}
+
+function showPets(snap) {
+  var data = snap.val();
+
+  var rows = "";
+  for (var key in data) {
+      rows += '<tr>' +
+          '<td>' + data[key].name + '</td>' +
+          '<td>' + data[key].school + '</td>' +
+          '<td>' + data[key].country + '</td>' +
+          '<td>' + data[key].email + '</td>' +
+          '<td>' +
+          '<i class="far fa-trash-alt delete" data-peti="' + key + '"></i>' +
+          '</td>' +
+          '</tr>';
+  }
+
+  var myTBody = document.getElementById("t-body-student");
+  myTBody.innerHTML = rows;
+
+  var deleteButtonsD = document.getElementsByClassName("delete");
+  for (var i = 0; i < deleteButtonsD.length; i++) {
+      deleteButtonsD[i].addEventListener("click", deletePeti);
+  }
+}
+
+function deletePeti(event) {
+  var buttonClicked = event.target;
+  var keyPetiToDelete = buttonClicked.getAttribute("data-peti");
+  var refPetiToDelete = firebase.database().ref("/subscriptions/" + keyPetiToDelete);
+  refPetiToDelete.remove();
+}
---
+++ public/js/signUp.js
@@ -0,0 +1,57 @@
+window.onload = initialize;
+
+function initialize(){
+loadFirebase();
+loadButtons();
+}
+
+function loadButtons(){
+    document.getElementById("submit-add-NS").addEventListener("submit", addNewStudent);
+}
+
+function loadFirebase() {
+    console.log("Firebase Loaded");
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyCWusAXBApIFx5YnC-hxSbQNl_tv0aOIK0",
+      authDomain: "aiki20.firebaseapp.com",
+      databaseURL: "https://aiki20.firebaseio.com",
+      projectId: "aiki20",
+      storageBucket: "aiki20.appspot.com",
+      messagingSenderId: "769004079783",
+      appId: "1:769004079783:web:93a8c53c46137655686982"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+  
+
+var errorSI = false;
+
+function addNewStudent(event) {
+    event.preventDefault();
+
+    var email = event.target.addNSEmail.value;
+    var password = event.target.addNSPwd.value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        errorSI = true;
+        console.log(errorSI);
+        alert("Error, Asegurese de que la contraseña tiene mas de 6 caracteres");
+        document.getElementById("mdl-add-good").style.display = "none";
+        errorSI = false;
+        return false;
+    });
+// Si hay un error va a ejecutarse esta parte de codigo de abajo , pero si hay error va a salir que se se creo correctamente
+// pero no es asi , asi que habra que hacer algo para que si hay error no lea esto
+    document.getElementById("mdl-add-good").style.display = "unset";
+    setTimeout("redirigir()",5000);
+}
+
+function redirigir(){
+    alert("jeje")
+    // aqui va el link de nuestra pagina de firebase
+}
